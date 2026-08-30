e899bfb52abdb1ab7ec0f0052a89e8d2b912cf05
TimelineTravelerSn86 <erfi.timeline@gmail.com>
2026-08-30 13:22:21 -0400

    Fixing LuckPerms and removing LocalPluginRevoking

--- plugins/sources.nix
+++ plugins/sources.nix
@@ -18,26 +18,10 @@ let
         cp $src $out/${name}-${version}.jar
       '';
     };
-  mkLocalPlugin =
-    {
-      name,
-      version,
-      srcPath,
-    }:
-    pkgs.stdenv.mkDerivation {
-      pname = name;
-      inherit version;
-      src = srcPath;
-      dontUnpack = true;
-      installPhase = ''
-        mkdir -p $out
-        cp $src $out/${name}-${version}.jar
-      '';
-    };
 
 in
 {
-  inherit mkPlugin mkLocalPlugin;
+  inherit mkPlugin;
 
   # Example set - swap in whatever you actually want.
   # Get each sha256 with: nix-prefetch-url <jar-download-url>
@@ -48,17 +32,12 @@ in
       url = "https://github.com/EssentialsX/Essentials/releases/download/2.20.1/EssentialsX-2.20.1.jar";
       sha256 = "0hpm3fk073f2z8aah9l1inq27h9kd60jb2c1grcs8326v85s6bl0";
     })
-    (mkPlugin {
-      name = "LuckPerms";
-      version = "5.4.130";
-      url = "https://github.com/LuckPerms/LuckPerms/releases/download/v5.4.130/LuckPerms-Bukkit-5.4.130.jar";
-      sha256 = lib.fakeSha256; # REPLACE
-    })
-    (mkLocalPlugin {
-      name = "Ping";
-      version = "1.0";
-      srcPath = ../plugins-src/ping/target/ping-plugin-1.0.jar;
-    })
+    # (mkPlugin {
+    #   name = "LuckPerms";
+    #   version = "5.4.130";
+    #   url = "https://github.com/LuckPerms/LuckPerms/releases/download/v5.4.130/LuckPerms-Bukkit-5.4.130.jar";
+    #   sha256 = lib.fakeSha256; # REPLACE
+    # })
 
     # Your custom /ping plugin from earlier, once built into a jar:
     # (mkPlugin {
