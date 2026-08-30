56479248b056e78f966503ad72c815674797cf96
TimelineTravelerSn86 <erfi.timeline@gmail.com>
2026-08-30 13:22:21 -0400

    Adding multiple useful plugins

--- plugins/sources.nix
+++ plugins/sources.nix
@@ -48,11 +48,36 @@ in
       version = "1.0";
       srcPath = ../plugins-src/ping/target/ping-plugin-1.0.jar;
     })
+
     (mkPlugin {
       name = "VoiceChat";
       version = "2.6.21";
       url = "https://cdn.modrinth.com/data/9eGKb6K1/versions/62MVmInV/voicechat-bukkit-2.6.21.jar";
       sha256 = "1n4lkckwyggp750dmxqrqhzf8mx3wabwh4qvi185bv0r1ivlb0f0";
+    })
+    (mkPlugin {
+      name = "Plan";
+      version = "5.8-build-3605";
+      url = "https://cdn.modrinth.com/data/wJQfHhxh/versions/VCtXebje/Plan-5.8-build-3605.jar";
+      sha256 = "0p0hkbpw07kkzq314lx7z12n0k7cnlzdadraxfnd5mqdj43bh77n";
+    })
+    (mkPlugin {
+      name = "CoreProtect";
+      version = "24.0";
+      url = "https://cdn.modrinth.com/data/Lu3KuzdV/versions/Kma0kBsY/CoreProtect-CE-24.0.jar";
+      sha256 = "15jn85h8v9s3l3qxb42mwv4hvgrknklpgvhql3jk115vi4h3dkb6";
+    })
+    (mkPlugin {
+      name = "PlayerStats";
+      version = "1.3";
+      url = "https://cdn.modrinth.com/data/ONtvwRCv/versions/8ng9KjEE/PlayerStats.jar";
+      sha256 = "0w88cbv5cq7sabbbahh7g7fhh5zi62p3m26iwgir8yd9smq0xpn1";
+    })
+    (mkPlugin {
+      name = "DiscordSRV";
+      version = "1.30.5";
+      url = "https://cdn.modrinth.com/data/UmLGoGij/versions/ATlquwiT/DiscordSRV-Build-1.30.5.jar";
+      sha256 = "1vzgi965afbp812zx0r6528zrw9kvnkr0w9b85vpqv0lxgra2bzg";
     })
 
     # Your custom /ping plugin from earlier, once built into a jar:
